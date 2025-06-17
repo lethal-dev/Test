49cd22e25e99e080e7497d5fb4bff7a6e552c787
lethal-dev <rafael.cardona@alumnos.udg.mx>
2025-06-17 12:48:49 -0600

    test aws key

--- main.py
+++ main.py
@@ -7,3 +7,5 @@ print('Cambios desde codespace merge o pull request')
 KEY = 'asdslkjsldkaksjflajshflajhsflaf'
 MY_API_KEY="elqueseriepierde1234"
 GITHUB_TOKEN = "ghp_1234567890abcdef1234567890abcdef1234"
+AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
